948d80f8a59045f2c7d3a210c95fbc2e81a09a08
ameen <shehaaa4@gmail.com>
2026-09-25 02:11:42 +0300

    chore: add .vercel to gitignore, remove build output from tracking

--- .vercel/output/functions/__fallback.func/chunks/virtual/styles.mjs
+++
@@ -1,7 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "../node_modules/nuxt/dist/app/entry.js": () => import('../build/entry-styles.DhT97_C6.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
